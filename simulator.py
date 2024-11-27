d009e50444bc81c4d369a87de137caab48f03a5b
unknown <sahartavakoli2002@gmail.com>
2024-11-27 23:31:49 +0330

    definition of classes(UML)

---
+++ simulator.py
@@ -0,0 +1,40 @@
+class Net:
+    def __init__(self):
+        self.number = 0
+        self.name = ''
+        self.value = 0
+
+
+class Gate:
+    def __init__(self):
+        type= ''
+        inputs = []
+        output = []
+        delay = 0
+
+
+class CircuitSimulator:
+    def __init__(self):
+        file_name = ''
+        gates = []
+        inputs = []
+        outputs = []
+        input_values = []
+        nets = []
+
+    
+    def read_isc_file(self):
+        pass
+
+    
+    def read_inputs(self):
+        pass
+
+
+    def true_value_simulation(self):
+        pass
+
+    
+    def simulation_with_delay(self):
+        pass
+
